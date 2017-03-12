2ab43aca3275dfb6c751e7e64abbb3c1f6e614f2
AUTplayed <fips.hem@gmail.com>
2017-03-12 15:26:11 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+//External Dependencies
+var express = require('express');
+var app = express();
+var path = require('path');
+
+//Internal Dependencies
+
+//Declarations
+
+app.use(express.static(__dirname + '/public'));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
